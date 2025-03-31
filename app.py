c40c94b1f06d9c7d378170262680e997fad8f52a
stevokwon <130387986+stevokwon@users.noreply.github.com>
2025-03-31 03:38:14 +0000

    Modified openai api key retrieval method

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import openai
 import numpy as np
 from numpy.linalg import norm 
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 app = Flask(__name__)
 
